ec17d6b445dcac5e51260c0ebd1d19cd1da9e5ed
josep <josep.almolda@gmail.com>
2019-02-04 16:53:51 +0000

    parser tests

---
+++ Notifications.Tests/BodyParser/ParseEventBody.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Moq;
+using Notifications.Common.Enums;
+using Notifications.Common.Interfaces;
+using Notifications.Common.Models;
+using Xunit;
+
+namespace Notifications.Tests.BodyParser
+{
+    public class ParseEventBody
+    {
+        [Fact]
+        public void WhenMethodCalledDataPropertiesAreInterpolated()
+        {
+            // Arrange
+            var data = new EventData
+            {
+                FirstName = "name",
+                Reason = "reason",
+                AppointmenDateTime = new DateTime(2020, 01, 02),
+                OrganisationName = "org"
+            };
+
+            const string body = "{FirstName}|{Reason}|{AppointmenDateTime}|{OrganisationName}";
+
+            var systemUnderTest = new Utils.BodyParser();
+
+            // Act
+            var result = systemUnderTest.ParseEventBody(body, data);
+
+            // Assert
+            var parts = result.Split('|');
+            Assert.Equal("name", parts[0]);
+            Assert.Equal("reason", parts[1]);
+            Assert.Equal(new DateTime(2020, 01, 02).ToShortDateString(), parts[2]);
+            Assert.Equal("org", parts[3]);
+        }
+
+        [Fact]
+        public void WhenMethodCalledDataPropertyIsInterpolatedMultipleTimes()
+        {
+            // Arrange
+            var data = new EventData
+            {
+                FirstName = "name",
+                Reason = "reason",
+                AppointmenDateTime = new DateTime(2020, 01, 02),
+                OrganisationName = "org"
+            };
+
+            const string body = "{FirstName}|{FirstName}|{FirstName}|{FirstName}";
+
+            var systemUnderTest = new Utils.BodyParser();
+
+            // Act
+            var result = systemUnderTest.ParseEventBody(body, data);
+
+            // Assert
+            var parts = result.Split('|');
+            Assert.Equal("name", parts[0]);
+            Assert.Equal("name", parts[1]);
+            Assert.Equal("name", parts[2]);
+            Assert.Equal("name", parts[3]);
+        }
+    }
+}
