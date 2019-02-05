d8a211857a9b45f4898e4b73dcd036a77468fc94
josep <josep.almolda@gmail.com>
2019-02-05 15:06:14 +0000

    add test

---
+++ Notifications.Tests/NotificationsService/GetNotificationsByUser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Moq;
+using Notifications.Common.Interfaces;
+using Xunit;
+
+namespace Notifications.Tests.NotificationsService
+{
+    public class GetNotificationsByUser
+    {
+        [Fact]
+        public void WhenMethodIsCalledDataAccessIsCalledWithTheRightId()
+        {
+            var userGuid = Guid.Parse("7c1e916c-6984-44b8-9778-c89704d08dda");
+            // Arrange
+
+            var mockDataAccess = new Mock<INotificationsAccess>();
+            var mockParser = new Mock<IBodyParser>();
+
+
+            var systemUnderTest = new Services.NotificationsService(mockDataAccess.Object, mockParser.Object);
+
+            // Act
+            systemUnderTest.GetNotificationsByUser(userGuid);
+
+            // Assert
+            mockDataAccess.Verify(access => access
+                .GetNotificationsById(It.Is<Guid>(x => x == userGuid)));
+
+        }
+
+    }
+
+}
