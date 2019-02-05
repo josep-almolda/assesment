beab1210c5804e77fc848a491c63070aec911356
josep <josep.almolda@gmail.com>
2019-02-05 12:52:41 +0000

    add migration for notification model

---
+++ Notifications.DataAccess/Migrations/20190205114710_notification.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Notifications.DataAccess.Migrations
+{
+    public partial class notification : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Text",
+                table: "Notifications",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Notifications",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Notifications",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Text",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Notifications");
+        }
+    }
+}
